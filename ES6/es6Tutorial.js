d9ae3ad605974ba7b0252cfb397a87c74b8acf7d
Filip Bošnjak <filipo.bosnjak@gmail.com>
2020-09-21 23:58:11 +0200

    Var vs Let...

--- ES6/es6Tutorial.js
+++ ES6/es6Tutorial.js
@@ -1,10 +1,18 @@
-//Variable scoping
+//Variable scoping and variables generally
 
 var counter = 5;
 
 function sayName(){
     var name = "Ed";
+    let sth  = 34
     console.log(name);
 }
 sayName();
-console.log(name);
+console.log(name);//Doesn't do anything because variable 'name' is declared localy
+console.log(sth);
+const pi = 3.14; //We can't reassign this variable. We would get an error for sth like 'pi = 4;'
+
+let counter = 10;
+counter = 15;
+
+//Var vs Let -> Var is available within the scope of the function and let is available within the scope of {} no matter what it was.
