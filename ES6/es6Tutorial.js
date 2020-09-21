6ae506b1e6a66f41319684b91a2d59f631b57631
Filip Bošnjak <filipo.bosnjak@gmail.com>
2020-09-22 00:11:20 +0200

    NeW Commit

--- ES6/es6Tutorial.js
+++ ES6/es6Tutorial.js
@@ -7,12 +7,18 @@ function sayName(){
     let sth  = 34
     console.log(name);
 }
+
 sayName();
-console.log(name);//Doesn't do anything because variable 'name' is declared localy
+
+console.log(name);//Doesn't do anything because variable 'name' is declared locally
 console.log(sth);
+
 const pi = 3.14; //We can't reassign this variable. We would get an error for sth like 'pi = 4;'
 
 let counter = 10;
-counter = 15;
+counter = 15;//This is perfectly legal
 
 //Var vs Let -> Var is available within the scope of the function and let is available within the scope of {} no matter what it was.
+//In for loop for(...){} we have curly brackets and its better to use let than var cause let wont get out of the scope of for loop and mess 
+//our code
+
