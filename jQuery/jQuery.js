9c84f78754b36b427f301967608db35aa658be71
Filip Bošnjak <filipo.bosnjak@gmail.com>
2020-09-23 00:55:55 +0200

    Adding more jQuery

--- jQuery/jQuery.js
+++ jQuery/jQuery.js
@@ -1,6 +1,14 @@
 //jQuery - Small, fast and feature-rich Javascript library
 //       - Makes DOM (document object model) manipulation, event handling and Ajax much easier
 
+// $ - Dollar sign - Represents jQuery
+
+// $('elementName').method();
+
 $('.btn').click( () => {
     $('h1').css('color','blue');
-})
+});
+
+//$('button').hide(); -> Hide button, its still in the html 
+
+//$('button#btnID')... if we had a button with the id="btnID" 
