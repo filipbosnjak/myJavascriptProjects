ca670dd74ba957163f887726ff7876c458bf5184
Filip Bošnjak <filipo.bosnjak@gmail.com>
2020-09-23 00:26:07 +0200

    New Commit

---
+++ jQuery/main.js
@@ -0,0 +1,6 @@
+//jQuery - Small, fast and feature-rich Javascript library
+//       - Makes DOM (document object model) manipulation, event handling and Ajax much easier
+
+$('.btn').click( () => {
+    $('h1').css('color','blue');
+})
