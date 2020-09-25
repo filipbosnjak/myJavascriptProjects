4fd9b575a4d128782d756a35c895699faf7fe40f
Filip Bošnjak <filipo.bosnjak@gmail.com>
2020-09-25 12:33:50 +0200

    Tuples, Functions & Arrays in TypeScript

--- TypeScript/types.js
+++ TypeScript/types.js
@@ -1,14 +1,34 @@
 //                          TYPESCRIPT
 //  Its a superset of javascript. Any valid javascript is valid typescript. Developed by Microsoft.
-//  What typescript offers us?
+//  What Typescript offers us?
 //  -> Static Type Checking - we re required to specify our types - like in for eg. Java or C - makes code more readable and descriptive
 //  -> Types: String, Number, Boolean, Array, Any, Void, Null, Tuple, Enum, Generics
 //  -> We can use classes and OOP - ES6 Syntax supported (yaay)
 //  -> Modularity - importing ts code
 //  -> Encapsulation with private and protected variables
 //  -> Inheritance
-//  Compile ts -> js | tsc types.ts | Npm installation 
-//  Js file is what we include in our project
-var mystring;
-mystring = 'Hello World';
-console.log(mystring);
+//  Compile ts -> js | tsc types.ts | Npm installation | Live compilation on save tsc types.ts -w (w for watch)
+//  Js file is what we include in our project | Always close js file while working on ts file
+/* -- TYPES IN TYPESCRIPT -- */
+var mystring = 'Hello World';
+var string1 = 'Hello';
+var string2 = 'World';
+// We can assign function return to a string
+var string3 = 'Filip'.slice(0, 3);
+console.log(string1 + ' ' + string2);
+console.log(string3);
+/* -- ARRAY OF STRINGS -- */
+var string4 = ['String1', 'String2', 'String3'];
+console.log(string4[1]);
+var numArr = [1, 2, 3];
+console.log(numArr);
+/* -- TUPLES -- */
+//Array with defined number and type of elements
+var tuple = ['Filip', 4];
+console.log(tuple);
+/* -- FUNCTIONS -- */
+var tsFunction = function (n1, n2) {
+    return n1 + n2;
+};
+//Function defined like this cant take anything other than number type variables
+console.log(tsFunction(2, 2));
--- TypeScript/types.ts
+++ TypeScript/types.ts
@@ -2,7 +2,7 @@
 
 //  Its a superset of javascript. Any valid javascript is valid typescript. Developed by Microsoft.
 
-//  What typescript offers us?
+//  What Typescript offers us?
 //  -> Static Type Checking - we re required to specify our types - like in for eg. Java or C - makes code more readable and descriptive
 //  -> Types: String, Number, Boolean, Array, Any, Void, Null, Tuple, Enum, Generics
 //  -> We can use classes and OOP - ES6 Syntax supported (yaay)
@@ -13,6 +13,44 @@
 //  Compile ts -> js | tsc types.ts | Npm installation | Live compilation on save tsc types.ts -w (w for watch)
 //  Js file is what we include in our project | Always close js file while working on ts file
 
+
+/* -- TYPES IN TYPESCRIPT -- */ 
+
 let mystring: String = 'Hello World';
- 
-console.log(mystring);
+let string1: String = 'Hello';
+let string2: String = 'World';
+
+// We can assign function return to a string
+
+let string3: String = 'Filip'.slice(0,3);
+
+console.log(string1 + ' ' + string2);
+console.log(string3);
+
+/* -- ARRAY OF STRINGS -- */
+
+let string4: Array<String> = ['String1' , 'String2' , 'String3'];
+
+console.log(string4[1]);
+
+let numArr: Array<Number> = [1,2,3];
+
+console.log(numArr);
+
+
+/* -- TUPLES -- */
+//Array with defined number and type of elements
+
+let tuple: [string,number] = ['Filip' , 4];
+
+console.log(tuple);
+
+/* -- FUNCTIONS -- */
+
+let tsFunction = (n1:number , n2:number) => {
+    return n1 + n2;
+}
+
+//Function defined like this cant take anything other than number type variables
+console.log(tsFunction(2,2));
+
